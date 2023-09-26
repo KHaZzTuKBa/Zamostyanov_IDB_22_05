5cbaed1f498da389e38ed6d145e44c8c4bdcf626
KHaZzTuKBa <zamock15@gmail.com>
2023-09-26 18:56:08 +0300

    1

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            inst();
+        }
+    }
+
+    void inst()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            Vector3 nv = new Vector3(i, 0, 0);
+            Instantiate(gameObject, nv, gameObject.transform.rotation);
+        }
     }
 }
